a1defdd5e43c3d3956dd0a28349da31a39619ed4
samurks <samueltorochagas@gmail.com>
2023-10-19 14:21:55 -0400

    atividade 19/10
    atualizado

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+const produtos = [
+  {id: 1, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 2, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 3, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 4, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 5, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 6, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 7, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 8, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 9, nome:"", preco:"", descricao:"", imagem:""},
+  {id: 10, nome:"", preco:"", descricao:"", imagem:""},
+]
+
 app.get('/', (req, res) => {
   res.render('index', { message: 'LISTA DE PRODUTOS' });
 });
