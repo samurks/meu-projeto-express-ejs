f633a6e7b468e19c990f908e63c46b4c4f1bc01e
samurks <samueltorochagas@gmail.com>
2023-11-16 14:11:12 -0400

    yiuy

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const produtos = [
-  {id: 1, nome:"", preco:"", descricao:"", imagem:""},
-  {id: 2, nome:"", preco:"", descricao:"", imagem:""},
-  {id: 3, nome:"", preco:"", descricao:"", imagem:""},
-  {id: 4, nome:"", preco:"", descricao:"", imagem:""},
-  {id: 5, nome:"", preco:"", descricao:"", imagem:""},
-  {id: 6, nome:"", preco:"", descricao:"", imagem:""},
-  {id: 7, nome:"", preco:"", descricao:"", imagem:""},
-  {id: 8, nome:"", preco:"", descricao:"", imagem:""},
-  {id: 9, nome:"", preco:"", descricao:"", imagem:""},
-  {id: 10, nome:"", preco:"", descricao:"", imagem:""},
-]
+  {id: 1, nome:"Tesla Coil", preco:"", descricao:"", imagem:""},
+  {id: 2, nome:"Vans Old Skool", preco:"", descricao:"", imagem:""},
+  {id: 3, nome:"Nike Dunk Low", preco:"", descricao:"", imagem:""},
+  {id: 4, nome:"Nike Tn", preco:"", descricao:"", imagem:""},
+  {id: 5, nome:"Nike Socks", preco:"", descricao:"", imagem:""},
+  {id: 6, nome:"Nike Jordan", preco:"", descricao:"", imagem:""},
+];
+
+function buscarProdutoPorId (id){
+  const produto = produtos.find(produto => produto.id == id);
+  return produto || null
+}
 
 app.get('/', (req, res) => {
   res.render('index', { message: 'LISTA DE PRODUTOS' });
