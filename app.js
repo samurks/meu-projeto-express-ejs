efc3af505716854c849feb2b63a83029a19c70a5
samurks <samueltorochagas@gmail.com>
2023-11-30 14:26:17 -0400

    renderizando pelo app.js
    codigo funcionando

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
+app.get('/', (req, res) => {
+  res.render('index', { produtos });
+});
+
 const produtos = [
-  {id: 1, nome:"Tesla Coil", preco:"359", descricao:"TENIS", imagem:"https://static3.tcdn.com.br/img/img_prod/901504/tenis_tesla_coil_ice_3231_1_9e2f5701065df2fa77ca9752019462a8.png"},
-  {id: 2, nome:"Vans Old Skool", preco:"399", descricao:"TENIS", imagem:"https://cdn.awsli.com.br/600x700/1702/1702086/produto/214885673bfa7fe5c09.jpg"},
-  {id: 3, nome:"Nike Dunk Low", preco:"1299", descricao:"TENIS", imagem:"https://cdn.awsli.com.br/600x1000/1167/1167039/produto/217818858/3-s3tk9bnles.jpg"},
-  {id: 4, nome:"Nike Tn", preco:"1359", descricao:"TENIS", imagem:"https://authenticfeet.vteximg.com.br/arquivos/ids/268207-600-600/DM003-2-003-1.jpg?v=637934896607100000"},
-  {id: 5, nome:"Nike Socks", preco:"3799", descricao:"TENIS", imagem:"https://cdn.awsli.com.br/600x1000/1918/1918121/produto/196654312/1-2d19ae200a.jpg"},
-  {id: 6, nome:"Nike Jordan", preco:"2000", descricao:"TENIS", imagem:"https://gringa.com.br/cdn/shop/products/Tenis-Nike-Air-Jordan-1-Retro-High-_Dark-Mocha_-36-Branco-Original00031998l_600x600_crop_center.png?v=1665494110"},
+  {id: 1, nome:"Tesla Coil", preco:"359", descricao:"Tenises de luxos de otima qualidade 100% oliginal, oferecido pela industria pastel de flango multinacionalmente reconhecida, por sempre entregar produtos de otima qualidade.", imagem:"/imagem/1-tesla.jpg"},
+  {id: 2, nome:"Vans Old Skool", preco:"399", descricao:"Tenises de luxos de otima qualidade 100% oliginal, oferecido pela industria pastel de flango multinacionalmente reconhecida, por sempre entregar produtos de otima qualidade.", imagem:"/imagem/2-vans.jpg"},
+  {id: 3, nome:"Nike Dunk Low", preco:"1299", descricao:"Tenises de luxos de otima qualidade 100% oliginal, oferecido pela industria pastel de flango multinacionalmente reconhecida, por sempre entregar produtos de otima qualidade.", imagem:"/imagem/3-dunk.jpg"},
+  {id: 4, nome:"Nike Tn", preco:"1359", descricao:"Tenises de luxos de otima qualidade 100% oliginal, oferecido pela industria pastel de flango multinacionalmente reconhecida, por sempre entregar produtos de otima qualidade.", imagem:"/imagem/4-tn.jpg"},
+  {id: 5, nome:"Nike Socks", preco:"3799", descricao:"Tenises de luxos de otima qualidade 100% oliginal, oferecido pela industria pastel de flango multinacionalmente reconhecida, por sempre entregar produtos de otima qualidade.", imagem:"/imagem/5-shocks.jpg"},
+  {id: 6, nome:"Nike Jordan", preco:"2000", descricao:"Tenises de luxos de otima qualidade 100% oliginal, oferecido pela industria pastel de flango multinacionalmente reconhecida, por sempre entregar produtos de otima qualidade.", imagem:"/imagem/6-jordan.jpg"},
+  {id: 7, nome:"Nike Air Max", preco:"999", descricao:"Tenises de luxos de otima qualidade 100% oliginal, oferecido pela industria pastel de flango multinacionalmente reconhecida, por sempre entregar produtos de otima qualidade.", imagem:"/imagem/7-airmax.jpg"},
+  {id: 8, nome:"Nike Air Force", preco:"789", descricao:"Tenises de luxos de otima qualidade 100% oliginal, oferecido pela industria pastel de flango multinacionalmente reconhecida, por sempre entregar produtos de otima qualidade.", imagem:"/imagem/8-airforce.jpg"},
 ];
 
 function buscarProdutoPorId (id){
@@ -19,13 +26,12 @@ function buscarProdutoPorId (id){
   return produto || null
 }
 
-app.get('/', (req, res) => {
-  res.render('index', { produtos });
+app.get('/produtos/:id', (req, res) => {
+  const produto = buscarProdutoPorId(req.params.id)
+  res.render('produto', {produto});
 });
 
-app.get('/produto', (req, res) => {
-  res.render('produto', { message: 'LISTA DE PRODUTOS' });
-});
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
